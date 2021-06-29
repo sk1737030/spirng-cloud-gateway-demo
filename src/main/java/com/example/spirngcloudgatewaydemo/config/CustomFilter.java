074df2c668ad215f00086fb702d3077db5b26bdc
dongle <dongle@r2v.io>
2021-06-29 22:55:21 +0900

    feat: Spring gateway 뼈대

---
+++ src/main/java/com/example/spirngcloudgatewaydemo/config/CustomFilter.java
@@ -0,0 +1,38 @@
+package com.example.spirngcloudgatewaydemo.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
+
+    public CustomFilter() {
+        super(Config.class);
+    }
+
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest(); // reactive를 import받아야한다 webflux를 지원한다 rxjava
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Custom PRE filter: request id -> {}", request.getId());
+
+            // Custom Post Filter
+            return chain.filter(exchange).then(Mono.fromRunnable(() ->
+                log.info("Custom Post filter: response code -> {}", response.getStatusCode())));
+        });
+    }
+
+    public static class Config {
+        // put the config properties
+    }
+
+}
---
+++ src/test/java/com/example/spirngcloudgatewaydemo/SpirngCloudGatewayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spirngcloudgatewaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngCloudGatewayDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
